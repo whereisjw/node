684b5c31cb91f08ef38baa029e8a6a994fed6713
whereisjw <j9399977@gmail.com>
2023-09-20 15:54:05 +0900

    20230920353

---
+++ code/14.stream_buffer/3.steram.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+//파일을 읽어오는 방법 --> Stream
+const readStream = fs.createReadSTream;
---
+++ code/14.stream_buffer/5.stream3.js
@@ -0,0 +1,2 @@
+//readSTream을 이용하여 file.txt 파일의 내용을 16바이트씩 읽어서
+//file-stream.txt 파일안에 writeStream을 이용하여 저장해주세요
